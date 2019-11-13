f280851a9a2d6d5a6710fd4e51f5b1d3acb1825e
Hyun Suk Ryoo <maxryoo@d-172-27-1-107.dhcp.virginia.edu>
2019-11-13 18:57:31 -0500

    xml request

--- location.js
+++ location.js
@@ -31,9 +31,8 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
-    document.getElementById("demo").innerHTML = myObj.name;
     console.log(myObj)
   }
 };
-xmlhttp.open("GET", "json_demo.txt", true);
+xmlhttp.open("GET", "location.json", true);
 xmlhttp.send();
