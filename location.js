89774936243d4548d160d94e38a99ab42ee139b5
Hyun Suk Ryoo <maxryoo@d-172-27-1-107.dhcp.virginia.edu>
2019-11-13 19:39:54 -0500

    add mapping

--- location.js
+++ location.js
@@ -1,39 +1,47 @@
-var locations = [
-    ['Bondi Beach', -33.890542, 151.274856, 4],
-    ['Coogee Beach', -33.923036, 151.259052, 5],
-    ['Cronulla Beach', -34.028249, 151.157507, 3],
-    ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-    ['Maroubra Beach', -33.950198, 151.259302, 1]
-];
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
-    console.log(myObj)
-    console.log(myObj["location"]["names"]);
+    var makemap = 0;
+    var locations = []
+    for (var i = 0; i < myObj[0]["location"]["names"].length; i++) {
+      if (i == myObj[0]["location"]["names"].length - 1) {
+        locations.push([myObj[0]["location"]["names"][i], 
+                      myObj[0]["location"]["latitude"][i], 
+                      myObj[0]["location"]["longitude"][i],
+                      myObj[0]["location"]["order"][i]])
+        makemap++;
+      }
+      else {
+        locations.push([myObj[0]["location"]["names"][i], 
+                      myObj[0]["location"]["latitude"][i], 
+                      myObj[0]["location"]["longitude"][i],
+                      myObj[0]["location"]["order"][i]])
+      }
+    }
+    if (makemap != 0) {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 2,
+        center: new google.maps.LatLng(39.8283, -98.5795),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+      var infowindow = new google.maps.InfoWindow();
+      var marker, i;
+      for (i = 0; i < locations.length; i++) { 
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+      }
+    }
+
   }
 };
 xmlhttp.open("GET", "location.json", true);
 xmlhttp.send();
-
-var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: new google.maps.LatLng(-33.92, 151.25),
-    // 39.8283° N, 98.5795° W
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-});
-var infowindow = new google.maps.InfoWindow();
-var marker, i;
-for (i = 0; i < locations.length; i++) { 
-    marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-    });
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-            infowindow.setContent(locations[i][0]);
-            infowindow.open(map, marker);
-        }
-    })(marker, i));
-}
-
